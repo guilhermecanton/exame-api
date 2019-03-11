b3ddd12dfe152ae1a64fe137026d2bfb3e633ee1
guilhermecanton <guilhermecantonx@gmail.com>
2019-03-11 09:14:55 -0300

    Update ConstantsUtils.java

--- src/main/java/br/com/digicade/exame/utils/ConstantsUtils.java
+++ src/main/java/br/com/digicade/exame/utils/ConstantsUtils.java
@@ -2,12 +2,6 @@ package br.com.digicade.exame.utils;
 
 public class ConstantsUtils {
 
-	public static final int TURNO_MANHA = 1;
-
-	public static final int TURNO_TARDE = 2;
-
-	public static final int TURNO_NOITE = 3;
-	
 	/**
 	 * Constante definida para
 	 * representar a origem das
